d9749537f3350f9de4395deb284c578a02c5b94c
gbasamera <595791422@qq.com>
2025-10-02 22:19:23 +0800

    初始化已完成，项目初始化包括：vite创建项目，路由初始化，pinia初始化 ElementPlus+Tailwindcss配置（TailwindCss配置有点问题，一部分样式无法完成设定）

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import router from './router/router.js';
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "./tailwindcss.css";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+Object.keys(ElementPlus).forEach((key) => {
+    app.component(key, ElementPlusIconsVue[key]);
+});
+
+const pinia = createPinia();
+app.use(router).use(pinia).use(ElementPlus);
+app.mount(`#app`);
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { useUserStore } from "@/store/user";
+  const user = useUserStore();
+</script>
+
+<template>
+  <div class="">主页</div>
+  <span>{{ user.token }}</span>
+<!--  <el-button type="primary">按钮</el-button>-->
+</template>
